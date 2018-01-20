c209be6f6da71ac2f3796d8db5cb43f71001127c
Trojan <ordrizzy@gmail.com>
2018-01-20 19:59:00 +0100

    Edits

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var express = require('express')
 var path = require('path')
 var favicon = require('serve-favicon')
+var validator = require('express-validator')
 
 //Initiate Express
 var app = express()
 
 //Define Desired Port Here
-const PORT =  3333
+const PORT = 3333
 
 //View Engine
 app.set('view engine', 'pug')
 
+//Set Public folder
 app.use(express.static('public'));
 
 //favicon
@@ -18,9 +20,11 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 //Define Routes Here
 var index = require('./routes/index')
+var form = require('./routes/form')
 
 //Add Routes Middlewares Here
-app.use('/', index);
+app.use('/', index)
+app.use('/form', form)
 
 //Middlewares Here
 app.use(express.static(path.join(__dirname, 'public')))
