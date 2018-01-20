808807ca719f7ba8e75a5187743b2885d303371d
Trojan <ordrizzy@gmail.com>
2018-01-20 15:31:20 +0100

    Middlewares Added

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var express = require('express')
+var path = require('path')
 var app = express()
 
+//Define Desired Port here
 const PORT =  3333
 
+//Define Routes Here
+var index = require('./routes/index')
+
+//Add Routes Middlewares Here
+app.use('/', index);
+
+//Middlewares Here
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Serve Application
 app.listen(PORT, (req, res, next) => {
   console.log('now serving on port ' + PORT)
 })
