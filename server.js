6d83b506ee4c9e91aaf26e6f089067ff0790f799
Trojan <ordrizzy@gmail.com>
2018-01-20 16:33:46 +0100

    Added Favicon support

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require('express')
 var path = require('path')
+var favicon = require('serve-favicon')
+
+//Initiate Express
 var app = express()
 
 //Define Desired Port here
 const PORT =  3333
+
+//favicon (would uncomment this when we have a stable page)
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 //Define Routes Here
 var index = require('./routes/index')
