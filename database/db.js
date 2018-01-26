4e64666a5183954fa9816bb560934c5695c4f2e5
Trojan <ordrizzy@gmail.com>
2018-01-26 13:29:22 +0100

    MongoDB Fun connection message

--- database/db.js
+++ database/db.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise
 
 // start connection
 mongoose.connect(process.env.DB_HOST).then(function (e){
-	console.log('Connected to MongoDB! 😃')
+	console.log('Connected to MongoDB! 😃🔥')
 }).catch(function (e){
 	console.error('Failed to connect to MongoDB 😕')
 })
