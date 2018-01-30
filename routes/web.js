c9137714b1af354b8df0312e468aee1c0a8e9834
Trojan <ordrizzy@gmail.com>
2018-01-30 11:01:51 +0100

    Useful comments

--- routes/web.js
+++ routes/web.js
@@ -1,17 +1,19 @@
-// import express
 let express = require('express')
 let app = express()
 let form = require('../controllers/form-controller')
 
+//get index.pug
 app.get('/', (req, res, next) => {
   res.render('index')
 })
 
+//get form.pug and pass the csrf token
 app.get('/form', (req, res, next) => {
   let csrfToken = req.csrfToken()
   res.render('form', { csrfToken })
 })
 
+//post request for form data
 app.post('/form', form.save)
 
 module.exports = app
