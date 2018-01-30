e7029176cfad98d20e7ffa140b7e5a27e187ea2c
Trojan <ordrizzy@gmail.com>
2018-01-30 18:31:54 +0100

    More Comments

--- routes/web.js
+++ routes/web.js
@@ -2,18 +2,18 @@ let express = require('express')
 let Route = express()
 let form = require('../controllers/form-controller')
 
-//get index.pug
+// Get index.pug
 Route.get('/', (req, res, next) => {
   res.render('index')
 })
 
-//get form.pug and pass the csrf token
+// Get form.pug and pass the csrf token
 Route.get('/form', (req, res, next) => {
   let csrfToken = req.csrfToken()
   res.render('form', { csrfToken })
 })
 
-//post request for form data
+// Post request for form data
 Route.post('/form', form.save)
 
 module.exports = Route
