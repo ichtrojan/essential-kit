8fd9840a82ab869b578087f667b77a4d6bf90a73
Trojan <ordrizzy@gmail.com>
2018-02-04 16:57:05 +0100

    Went back to default

--- database/db.js
+++ database/db.js
@@ -6,7 +6,5 @@ mongoose.Promise = global.Promise
 mongoose.connect(process.env.DB_HOST).then(function (e){
 	console.log('Connected to MongoDB! 😃🔥')
 }).catch(function (e){
-	console.error(
-		'Failed to connect to MongoDB 😕💥 \nCheck your DB_HOST environment variable then restart the server'
-	)
+	console.error('Failed to connect to MongoDB 😕💥 ')
 })
