f4aabd7322c5256925087aaf6480b2186a6b67ce
Trojan <ordrizzy@gmail.com>
2018-02-04 16:56:53 +0100

    First step to making Auth

---
+++ controllers/auth/user-controller.js
@@ -0,0 +1,12 @@
+// Require mongoose
+let mongoose = require('mongoose')
+let User = require('../../models/user')
+
+// Save user
+exports.signup = (req, res, next) => {
+
+}
+
+exports.login = (req, res, next) => {
+
+}
